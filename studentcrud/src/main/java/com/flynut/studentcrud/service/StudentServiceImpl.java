67dbf9d7566ff2b480946f60c5e7187033977834
Shubh9377 <mahajanshubham103@gmail.com>
2025-01-08 14:02:57 +0530

    First Project Commit

--- studentcrud/src/main/java/com/flynut/studentcrud/service/StudentServiceImpl.java
+++ studentcrud/src/main/java/com/flynut/studentcrud/service/StudentServiceImpl.java
@@ -3,10 +3,12 @@ package com.flynut.studentcrud.service;
 import com.flynut.studentcrud.entity.Student;
 import com.flynut.studentcrud.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class StudentServiceImpl implements StudentService{
 
     @Autowired
